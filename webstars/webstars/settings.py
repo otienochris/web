a9eac8dd136dfeb00af84e55e9617668861380a6
otienochris <otienochris98@gmail.com>
2019-01-17 00:27:06 +0300

    changed the nav bar

--- webstars/webstars/settings.py
+++ webstars/webstars/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'users',
+    'users.apps.UsersConfig',
     'crispy_forms',
     'webstarsapp',
     'django.contrib.admin',
